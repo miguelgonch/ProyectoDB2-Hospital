82ff5c648b9eef2ab78063d7c3071d8537a3a60a
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-02-26 23:43:26 -0600

    java

---
+++ src/java/utils/DataDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import bean.UserAccount;
+import config.SecurityConfig;
+
+/**
+ *
+ * @author migue
+ */
+public class DataDAO {
+    private static final Map<String, UserAccount> mapUsers = new HashMap<String, UserAccount>();
+
+    static {
+      initUsers();
+    }
+
+    private static void initUsers() {
+
+      // This user has a role as EMPLOYEE.
+      UserAccount emp = new UserAccount("employee1", "123", UserAccount.GENDER_MALE, //
+            SecurityConfig.ROLE_EMPLOYEE);
+
+      // This user has 2 roles EMPLOYEE and MANAGER.
+      UserAccount mng = new UserAccount("manager1", "123", UserAccount.GENDER_MALE, //
+            SecurityConfig.ROLE_EMPLOYEE, SecurityConfig.ROLE_MANAGER);
+
+      mapUsers.put(emp.getUserName(), emp);
+      mapUsers.put(mng.getUserName(), mng);
+    }
+
+    // Find a User by userName and password.
+    public static UserAccount findUser(String userName, String password) {
+      UserAccount u = mapUsers.get(userName);
+      if (u != null && u.getPassword().equals(password)) {
+         return u;
+      }
+      return null;
+    }
+}
