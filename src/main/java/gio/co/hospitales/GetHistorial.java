fc21e1d2bb8eb8fe5cad4d194293f1e011b7d935
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-03-11 13:13:30 -0600

    historial funcionando

--- src/main/java/gio/co/hospitales/GetHistorial.java
+++ src/main/java/gio/co/hospitales/GetHistorial.java
@@ -9,45 +9,69 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import oracle.jdbc.OraclePreparedStatement;
 import oracle.jdbc.OracleResultSet;
 
 /**
- * Servlet implementation class GetCitas
+ * Servlet implementation class GetPatient
  */
-@WebServlet("/GetCitas")
-public class GetCitas extends HttpServlet {
+@WebServlet("/GetPatient")
+public class GetPatient extends HttpServlet {
 	private static final long serialVersionUID = 1L;
         private static String hospitalNum = null;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public GetCitas() {
+    public GetPatient() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// Generar jsons
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+                //Obtener # del hospital
                 getInfoCookies(request,response);
+                //Conexion con db oracle
                 Connection conn = gio.co.hospitales.JavaConnectDb.connectDbH(Integer.parseInt(hospitalNum));
-                response.setContentType("text/html");
+                //Response info
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
                 PrintWriter out = response.getWriter();
                 try{
-                    String sql = "select * from citas";
+                    //Query
+                    String sql = "select paciente_id,nombre,apellido,tel,dpi,num_seguro from pacientes order by paciente_id";
                     OraclePreparedStatement pst = (OraclePreparedStatement) conn.prepareStatement(sql);
-                    OracleResultSet rs = (OracleResultSet) pst.executeQuery();
-                    String outPutTable = "<table><thead><tr><th>ID</th><th>Diagnostico</th><th>Resultados</th><th>Medicinas</th><th>Pasos a seguir</th><th>Observaciones</th><th>Fecha</th></tr></thead>";
+                    OracleResultSet rs = (OracleResultSet) pst.executeQuery();                    
+                    //Array jsons
+                    JSONArray jArray = new JSONArray();
+                    int i = 0;
                     while(rs.next()){
-                        outPutTable += "<tr><td>"+rs.getString(1)+"</td>"+"<td>"+rs.getString(3)+"</td>"+"<td>"+rs.getString(4)+"</td>"+"<td>"+rs.getString(5)+"</td>"+"<td>"+rs.getString(6)+"</td>"+"<td>"+rs.getString(7)+"</td>"+"<td>"+rs.getString(8)+"</td></tr>";
+                        //obtener parametros
+                        String id = rs.getString("PACIENTE_ID");
+                        String name = rs.getString("Nombre");
+                        String lastN = rs.getString("Apellido");
+                        String tel = rs.getString("TEL");
+                        String dpi = rs.getString("DPI");
+                        String segNum = rs.getString("num_seguro");
+                        //Crear objeto json
+                        JSONObject arrayObj = new JSONObject();
+                        arrayObj.put("id",id);
+                        arrayObj.put("nombre",name);
+                        arrayObj.put("apellido",lastN);
+                        arrayObj.put("tel",tel);
+                        arrayObj.put("dpi",dpi);
+                        arrayObj.put("segNum",segNum);
+                        //insertar objeto a array jsons
+                        jArray.add(i,arrayObj);
+                        i++;
                     }
-                    outPutTable+="</table>";
-                    out.println(outPutTable);
+                    rs.close ();
+                    pst.close ();
                     conn.close();
+                    out.print(jArray);
                 }catch(Exception e){
                     System.err.println(e);
                 }
