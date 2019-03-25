f9831b46e3cee19196ba75a6708cd3ed692baf30
manupianista <manuelecastilloo@gmail.com>
2019-03-25 16:54:08 -0600

    crud bueno menos update funcional

---
+++ src/main/webapp/js/especialidadInf.js
@@ -0,0 +1,24 @@
+$(document).ready(
+        function() {
+            var $idNum = $('#idNum');
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:8080/proyectoDB2-Hospitales/DocInfo',
+                dataType: 'json',
+                success: function(data) {
+                    var $pData = $('#doctoresData');
+                    $pData.empty();
+                    for (var i = 0; i < data.length; i++) {
+                        $pData.append("<option value="+data[i].id+">"+data[i].name+"</option>");
+                    }
+                    if(data.length==0){
+                        $pData.append("<p>No hay datos disponibles</p>");
+                    }
+                },
+                error : function() {
+                    var $pData = $('#doctoresData');
+                    $pData.empty();
+                    $pData.append("<p>No hay datos disponibles</p>");
+                }
+            });
+});
---
+++ src/main/webapp/js/typeInf.js
@@ -0,0 +1,24 @@
+$(document).ready(
+        function() {
+            var $idNum = $('#idNum');
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:8080/proyectoDB2-Hospitales/AsegInfo',
+                dataType: 'json',
+                success: function(data) {
+                    var $pData = $('#asegData');
+                    $pData.empty();
+                    for (var i = 0; i < data.length; i++) {
+                        $pData.append("<option value="+data[i].aId+">"+data[i].aName+"</option>");
+                    }
+                    if(data.length==0){
+                        $pData.append("<p>No hay datos disponibles</p>");
+                    }
+                },
+                error : function() {
+                    var $pData = $('#doctoresData');
+                    $pData.empty();
+                    $pData.append("<p>No hay datos disponibles</p>");
+                }
+            });
+});
