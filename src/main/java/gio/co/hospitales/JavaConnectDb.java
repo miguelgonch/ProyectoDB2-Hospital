5f00c8758a3b259374e2268d4d06784bc5e6b953
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-04-01 15:50:52 -0600

    connectdb h3

--- src/main/java/gio/co/hospitales/JavaConnectDb.java
+++ src/main/java/gio/co/hospitales/JavaConnectDb.java
@@ -5,33 +5,32 @@ import java.sql.DriverManager;
 //import java.sql.SQLException;
 
 public class JavaConnectDb {
-	public static Connection connectDbH(int num) {
-		Connection conn = null;
-		int numHospital = num;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			switch(numHospital) {
-                            case 1:
-                                    conn = DriverManager.getConnection("jdbc:oracle:thin:@25.66.75.32:1521:XE","c##hospital1","adminm");
-                                    break;
-                            case 2:
-                                    //conexion del abue
-                                    conn = DriverManager.getConnection("jdbc:oracle:thin:@25.65.236.60:1521:orcl","c##hospital2","adminm");
-                                    break;
-                            case 3:
-                                    //conexion de manu
-                                    conn = DriverManager.getConnection("jdbc:oracle:thin:@25.74.104.162:1521:xe","c##hospital3","manu");
-                                    break;
-                            default:
-                                    //por defecto es al hospital 1
-                                    conn = DriverManager.getConnection("jdbc:oracle:thin:@25.66.75.32:1521:XE","c##hospital1","adminm");
-                                    break;
-			}
-		} catch(Exception e){
-			System.err.println(e);
-		}
-		return conn;
-	}
+
+    public static Connection connectDbH(int num) {
+        Connection conn = null;
+        int numHospital = num;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            switch (numHospital) {
+                case 1:
+                    //conexion de manu
+                    conn = DriverManager.getConnection("jdbc:oracle:thin:@25.74.104.162:1521:xe", "c##hospital3", "manu");
+                    break;
+                case 2:
+                    //conexion del abue
+                    conn = DriverManager.getConnection("jdbc:oracle:thin:@25.65.236.60:1521:orcl", "c##hospital2", "adminm");
+                    break;
+                case 3:
+                    conn = DriverManager.getConnection("jdbc:oracle:thin:@25.66.75.32:1521:XE", "c##hospital1", "adminm");
+                    break;
+                default:
+                    //por defecto es al hospital 1
+                    conn = DriverManager.getConnection("jdbc:oracle:thin:@25.66.75.32:1521:XE", "c##hospital1", "adminm");
+                    break;
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        return conn;
+    }
 }
-
-
