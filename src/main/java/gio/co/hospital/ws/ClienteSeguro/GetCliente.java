b872839bfaf59864298e27aa2dc40d7875592a34
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-04-18 17:07:44 -0600

    Hacer que funcione get cliente con localhost

--- src/main/java/gio/co/hospital/ws/ClienteSeguro/GetCliente.java
+++ src/main/java/gio/co/hospital/ws/ClienteSeguro/GetCliente.java
@@ -46,8 +46,10 @@ public class GetCliente extends HttpServlet {
             if ((parClienteId != null) && !(parClienteId.equals(""))) {
                 int cId = Integer.parseInt(request.getParameter("dpi"));
                 url = "http://25.65.236.60:8080/proyectoDB2-seguro/restC/cliente/getCliente?dpi=" + cId;
+                //url = "http://localhost:8080/proyectoDB2-seguro/restC/cliente/getCliente?dpi=" + cId;
             } else {
                 url = "http://25.65.236.60:8080/proyectoDB2-seguro/restC/cliente/getCliente/";
+                //url = "http://localhost:8080/proyectoDB2-seguro/restC/cliente/getCliente/";
             }
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
