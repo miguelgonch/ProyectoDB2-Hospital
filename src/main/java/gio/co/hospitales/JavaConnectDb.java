652c52435cbaf3494154ec83d30c1e91c808b402
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-04-21 21:51:59 -0600

    scripts abue

--- src/main/java/gio/co/hospitales/JavaConnectDb.java
+++ src/main/java/gio/co/hospitales/JavaConnectDb.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 
 public class JavaConnectDb {
 
-    public static int hospNum = 2;
+    public static int hospNum = 1;
 
     public static int getHospNum() {
         return hospNum;
@@ -38,6 +38,7 @@ public class JavaConnectDb {
             System.err.println(e);
         }
         return conn;
+
     }
 }
 
