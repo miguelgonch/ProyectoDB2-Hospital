1e21e54df94e3321368ccb36696a4f2e0413f663
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2019-04-22 00:18:50 -0600

    cambio en java connect para que hospnum sea2

--- src/main/java/gio/co/hospitales/JavaConnectDb.java
+++ src/main/java/gio/co/hospitales/JavaConnectDb.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 
 public class JavaConnectDb {
 
-    public static int hospNum = 1;
+    public static int hospNum = 2;
 
     public static int getHospNum() {
         return hospNum;
@@ -41,5 +41,6 @@ public class JavaConnectDb {
 
     }
 }
+
 
 
