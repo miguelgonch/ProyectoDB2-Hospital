ae5535dae63a3969ccc210cf8787f2318231d869
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-09-20 17:21:24 -0600

    cambiar a la conexion hospital 1

--- src/main/java/gio/co/hospitales/JavaConnectDb.java
+++ src/main/java/gio/co/hospitales/JavaConnectDb.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 
 public class JavaConnectDb {
 
-    public static int hospNum = 3;
+    public static int hospNum = 1;
 
     public static int getHospNum() {
         return hospNum;
