6760785ae9fc44802d0099ec5ef2aabaab94599d
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-10-22 00:09:34 -0600

    prometheus WS

---
+++ src/main/java/gio/co/hospital/ws/prometheus/Prometheus.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gio.co.hospital.ws.prometheus;
+
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author migue
+ */
+@Path("/")
+public class Prometheus {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of Prometheus
+     */
+    public Prometheus() {
+    }
+
+    /**
+     * Retrieves representation of an instance of gio.co.hospital.ws.prometheus.Prometheus
+     * @return Response type
+     */
+    @GET
+    public Response getPrometheus() {
+        PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        return Response.status(200).entity(prometheusRegistry.scrape().getBytes()).build();
+
+    }
+}
